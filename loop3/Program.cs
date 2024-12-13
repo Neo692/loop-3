8ed6e92f1967ae3470f46db857696d7cc6afbac2
nehy06001 <nehy06001@E-PW05CGQV>
2024-12-13 08:52:55 +0100

    Add project files.

---
+++ loop3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace loop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("Skriv in fem heltal");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Skriv in ett heltal");
+                int nummer = int.Parse(Console.ReadLine());
+                sum += nummer;
+            }
+
+            Console.WriteLine($"Summan av talen är: {sum}");
+        }
+    }
+}
